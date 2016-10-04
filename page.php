d4a6db018be12ef1a11c42fb702d30524899dd60
Wes Kempfer <weskempferjr@Wess-MacBook-Pro.local>
2016-10-04 00:38:56 -0700

    Layout refinements

--- page.php
+++ page.php
@@ -13,26 +13,36 @@
  */
 
 get_header(); ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+					<?php
+					while ( have_posts() ) : the_post();
 
+						get_template_part( 'template-parts/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+
+					endwhile; // End of the loop.
+					?>
+
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		</div> <!-- col -->
+
+		<div class="col-md-4">
 			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
+			get_sidebar();
 			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+		</div>
+	</div> <!-- row -->
+</div> <!-- container -->
 <?php
-get_sidebar();
+
 get_footer();

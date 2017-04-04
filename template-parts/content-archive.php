88757520338d40999174fa912a9ae67eed891f54
Wes Kempfer <weskempferjr@Wess-MacBook-Pro.local>
2017-04-03 21:08:30 -0700

    Display add to cart at top and bottom of product pages

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -64,17 +64,13 @@ else {
 			<?php
             
             if ( $show_full_text === false ) {
-            
-                the_excerpt( sprintf(
-                /* translators: %s: Name of current post. */
-                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'pad' ), array( 'span' => array( 'class' => array() ) ) ),
-                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
-                ), true);
-    
+
+                the_excerpt();
+
                 wp_link_pages( array(
-                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pad' ),
-                    'after'  => '</div>',
-                ) );
+                        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pad' ),
+                        'after'  => '</div>',
+                    ) );
                 
             }
             else {
